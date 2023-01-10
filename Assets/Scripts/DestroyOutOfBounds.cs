d98ddb6524836e117ff375c929b5ba8ba5741ca5
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2023-01-10 12:36:56 -0500

    score

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,8 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
+    float sideBound = 25;
+    GameManager gM;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,12 @@ public class DestroyOutOfBounds : MonoBehaviour
       else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
-            Debug.Log("Game Over!");
+            gM.UpdateLives(-1);
         }
+      if (transform.position.x < -sideBound || transform.position.x > sideBound)
+        {
+            Destroy(gameObject);
+            gM.UpdateLives(-1);
+        }  
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int score = 0;
+    int lives = 10;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Score: " + score + "\n" + "Lives: " + lives);
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateLives(int value)
+    {
+        lives += value;
+        if(lives < 1)
+        {
+            Debug.Log("Out of Lives");
+            lives = 0;
+        }
+        Debug.Log("Lives: " + lives);
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        Debug.Log("Score: " + score);
+    }
+}
